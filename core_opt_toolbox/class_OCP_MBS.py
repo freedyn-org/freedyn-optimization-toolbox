ad9cc574b38aebb3a5d19abaf1408984e6088746
Zallinger Philipp <philipp.zallinger@fh-wels.at>
2026-05-15 14:06:31 +0200

    first commit in open source repository

--- core_opt_toolbox/class_OCP_MBS.py
+++ core_opt_toolbox/class_OCP_MBS.py
@@ -62,8 +62,8 @@ class Optimization(Control, FreeDyn, MBS_SysMat, BDF, adjGrads, numDiff, fcts_Us
             
             self.write_ctrl_dataSPL()
             
-            self.fd_model.__del__()
-            self.fd_model = fd.Model(self.fds_path_name, status_output="NO")
+            # self.fd_model.__del__()
+            # self.fd_model = fd.Model(self.fds_path_name, status_output="NO")
             
             self.update_ctrl_gridNodes()
             self.fd_model.reset_for_rerun()
