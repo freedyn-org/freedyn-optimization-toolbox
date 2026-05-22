8b2338093caf58754e38c5ce8de81a10393c6a4d
Zallinger Philipp <philipp.zallinger@fh-wels.at>
2026-05-22 09:57:54 +0200

    first commit in open source repository

--- examples/example_01_two_mass_oscillator_trajectory_tracking/main_TwoMass.py
+++ examples/example_01_two_mass_oscillator_trajectory_tracking/main_TwoMass.py
@@ -41,18 +41,18 @@ path_fds = path_main
 name_fds = 'OptCtrl_TwoMass'
 
 # Define FreeDyn data object spline of the controls
-nameCtrlSpline = ["uDach"]
+name_ctrlSPL = ["uDach"]
 
 # Define FreeDyn parameter for fdu
-nameParFdu = ["fdu"]
+name_fDu_par = ["fdu"]
 #
 # -----------------------------------------------------------------------------
 #
 """ Define controls """
-numControls = 1     # number of controls
-numGridNodes = 22   # number of grid nodes per control
+num_ctrls = 1     # number of controls
+num_ctrl_gridNodes = 22   # number of grid nodes per control
 
-uDachInit = np.zeros(numGridNodes*numControls)
+uDachInit = np.zeros(num_ctrl_gridNodes*num_ctrls)
 #
 # -----------------------------------------------------------------------------
 #
@@ -63,9 +63,9 @@ xF = np.array([])   # final constraints,
 #
 # -----------------------------------------------------------------------------
 #
-""" Define initial values for optimization variables z0"""
-z0 = uDachInit      
-numOptVar = len(z0)
+""" Define initial values for optimization variables zInit"""
+zInit = uDachInit      
+num_optVars = len(zInit)
 #
 # -----------------------------------------------------------------------------
 #
@@ -78,10 +78,10 @@ if tF_free:
 else:
     from class_OCP_MBS import Optimization
 
-optim = Optimization(numOptVar, numControls, numGridNodes,
+optim = Optimization(num_optVars, num_ctrls, num_ctrl_gridNodes,
                      tF, xF,
                      path_fds, name_fds,
-                     nameCtrlSpline, nameParFdu,
+                     name_ctrlSPL, name_fDu_par,
                      path_FDdll)
 #
 # -----------------------------------------------------------------------------
@@ -89,7 +89,7 @@ optim = Optimization(numOptVar, numControls, numGridNodes,
 """  Set up of the optimization-toolbox """
 # Add or comment out – according to the optimization problem
 res = sp.optimize.minimize(fun         = optim.objective,                # cost function
-                           x0          = z0,                             # initial values
+                           x0          = zInit,                             # initial values
                            method      = 'SLSQP',                        # optimization method
                            jac         = optim.get_grad_J,               # gradient of cost function
                            # bounds      = sp.optimize.Bounds(lb, ub),     # lower and upper bounds
@@ -107,20 +107,19 @@ res = sp.optimize.minimize(fun         = optim.objective,                # cost 
 #
 """ Update optimization variables in class and rerun simulation """
 optim.update_vars_if_changed(res.x)
-optim.change_tF_in_fds(optim.fds_path_name_optimized)
 optim.write_ctrl_dataSPL()
 #
 # -----------------------------------------------------------------------------
 #
 """ Get data for plots """
-t = np.zeros(optim.numTimeSteps)
-tau = np.zeros(optim.numTimeSteps)
-uInit = np.zeros((numControls, optim.numTimeSteps))
-u = np.zeros((numControls, optim.numTimeSteps))
-y = np.zeros(optim.numTimeSteps)
-ybar = np.zeros(optim.numTimeSteps)
+t = np.zeros(optim.num_time_steps)
+tau = np.zeros(optim.num_time_steps)
+uInit = np.zeros((num_ctrls, optim.num_time_steps))
+u = np.zeros((num_ctrls, optim.num_time_steps))
+y = np.zeros(optim.num_time_steps)
+ybar = np.zeros(optim.num_time_steps)
 
-for i in range(optim.numTimeSteps-1, -1, -1): 
+for i in range(optim.num_time_steps-1, -1, -1): 
    optim.fd_model.fetch_states_at_index(i)
    t[i] = optim.fd_model.t
    tau[i] = t[i]/optim.tF
